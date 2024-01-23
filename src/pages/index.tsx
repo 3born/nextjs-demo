fa0192e05d723d5f052f410991409335dcf3213f
Fredrik Fridborn <fredrik.fridborn@gmail.com>
2024-01-23 16:32:54 +0100

    break stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,10 @@ export default function Home() {
         <div className="home">
             <p>Current route: /</p>
             {/* <AdkContainer id="native-marking" /> */}
-            {/* <AdkContainer id="native-article" /> */}
+            <AdkContainer id="native-article" />
             {/* <AdkContainer id="native-scroll" /> */}
             {/* <AdkContainer id="native-teaser" /> */}
-            <AdkContainer id="my-container" />
+            {/* <AdkContainer id="my-container" /> */}
         </div>
     )
 }
